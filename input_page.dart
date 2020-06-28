7d2f99062a5abd583faa50ddfd44b173efa443ab
snehagupta01 <43209043+snehagupta01@users.noreply.github.com>
2020-06-28 15:09:21 +0530

    added main page

---
+++ input_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'reusable_dart.dart';
+import 'icon_content.dart';
+
+const activeCardColor = Color(0xFF1D1E33);
+const inactiveCardColor = Color(0xFF1D1E28);
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Color card1 = inactiveCardColor;
+  Color card2 = inactiveCardColor;
+  Color card3 = inactiveCardColor;
+  Color card4 = inactiveCardColor;
+  Color card5 = inactiveCardColor;
+  Color card6 = inactiveCardColor;
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Ecoville'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {
+                      //print("heyya");
+                    },
+                    child: ReusableCard(
+                      colour: card1,
+                      cardChild: IconContent(
+                        label: '1',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ReusableCard(
+                      colour: card2,
+                      cardChild: IconContent(
+                        label: '2',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ReusableCard(
+                      colour: card3,
+                      cardChild: IconContent(
+                        label: '3',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ReusableCard(
+                      colour: card4,
+                      cardChild: IconContent(
+                        label: '4',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ReusableCard(
+                      colour: card5,
+                      cardChild: IconContent(
+                        label: '5',
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: ReusableCard(
+                      colour: card6,
+                      cardChild: IconContent(
+                        label: '6',
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
